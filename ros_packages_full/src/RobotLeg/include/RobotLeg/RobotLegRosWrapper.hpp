772264f834d5815363aafc65752b3f38fdc093d4
eSqadron <kubam0906@gmail.com>
2022-10-07 20:38:19 +0200

    Robot Leg cpp to hpp and cpp split

---
+++ ros_packages_full/src/RobotLeg/include/RobotLeg/RobotLegRosWrapper.hpp
@@ -0,0 +1,46 @@
+//
+// Created by OEM on 07.10.2022.
+//
+
+#ifndef ROBOTLEG_ROBOTLEGROSWRAPPER_HPP
+#define ROBOTLEG_ROBOTLEGROSWRAPPER_HPP
+
+
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "maestro_interfaces/msg/maestro_target.hpp"
+#include "maestro_interfaces/msg/current_positions.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class RobotLegRosWrapper : public rclcpp::Node{
+public:
+    RobotLegRosWrapper() : Node("robot_leg_ros_wrapper"), count_(0){
+        publisher_ = this->create_publisher<maestro_interfaces::msg::MaestroTarget>("maestro_target", 10);
+        timer_ = this->create_wall_timer(500ms, std::bind(&RobotLegRosWrapper::timer_callback, this));
+    }
+private:
+
+    int target_i = 4*496;
+    int sweep_dir = 1;
+
+    void timer_callback();
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<maestro_interfaces::msg::MaestroTarget>::SharedPtr publisher_;
+    size_t count_;
+};
+
+
+
+
+
+#endif //ROBOTLEG_ROBOTLEGROSWRAPPER_HPP
--- ros_packages_full/src/RobotLeg/src/RobotLegRosWrapper.cpp
+++ ros_packages_full/src/RobotLeg/src/RobotLegRosWrapper.cpp
@@ -2,41 +2,7 @@
 // Created by OEM on 05.10.2022.
 //
 
-//#include "../include/RobotLeg/RobotLegRosWrapper.hpp"
-
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <string>
-
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "maestro_interfaces/msg/maestro_target.hpp"
-#include "maestro_interfaces/msg/current_positions.hpp"
-
-using namespace std::chrono_literals;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
-
-class RobotLegRosWrapper : public rclcpp::Node{
-public:
-    RobotLegRosWrapper() : Node("robot_leg_ros_wrapper"), count_(0){
-        publisher_ = this->create_publisher<maestro_interfaces::msg::MaestroTarget>("maestro_target", 10);
-        timer_ = this->create_wall_timer(500ms, std::bind(&RobotLegRosWrapper::timer_callback, this));
-    }
-private:
-
-    int target_i = 4*496;
-    int sweep_dir = 1;
-
-    void timer_callback();
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<maestro_interfaces::msg::MaestroTarget>::SharedPtr publisher_;
-    size_t count_;
-};
-
-
+#include "../include/RobotLeg/RobotLegRosWrapper.hpp"
 
 void RobotLegRosWrapper::timer_callback(){
     auto message = maestro_interfaces::msg::MaestroTarget();
