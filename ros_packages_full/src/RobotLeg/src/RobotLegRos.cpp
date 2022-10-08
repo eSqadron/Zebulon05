b476f704accb9ea8ecbc8bb5fbe67d6c21f92308
eSqadron <kubam0906@gmail.com>
2022-10-08 23:54:08 +0200

    Update RobotLegRos.cpp

--- ros_packages_full/src/RobotLeg/src/RobotLegRos.cpp
+++ ros_packages_full/src/RobotLeg/src/RobotLegRos.cpp
@@ -6,14 +6,8 @@
 
 
 void RobotLegRos::do_step(int step_length, int step_height){
-    if(sweep_dir == 1) {
         this->get_servos_to_pos(std::array<unsigned int, 3>({2496 * 4, 2496 * 4, 2496 * 4 * step_length * step_height}));
-        sweep_dir = -1;
-    }
-    else {
         this->get_servos_to_pos(std::array<unsigned int, 3>({496 * 4, 496 * 4, 496 * 4 * step_length * step_height}));
-        sweep_dir = 1;
-    }
 }
 
 void RobotLegRos::get_servos_to_pos(std::array<unsigned int, 3> new_servo_pos){
