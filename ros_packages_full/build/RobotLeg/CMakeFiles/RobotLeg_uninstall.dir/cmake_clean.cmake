3df03a1fc98d4d6fc011327f0409480525f1e32b
eSqadron <kubam0906@gmail.com>
2022-10-14 20:28:49 +0200

    test

--- ros_packages_full/build/RobotLeg/CMakeFiles/RobotLeg_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/RobotLeg_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RobotLeg_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ros_packages_full/build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/k/Desktop/Zeb05/Zebulon05/ros_packages_full/src/ros_tutorials/turtlesim
-# Build directory: /home/k/Desktop/Zeb05/Zebulon05/ros_packages_full/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
