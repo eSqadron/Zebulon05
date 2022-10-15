dcef8a24cd2c851c05ce488ec2a6dbe840b85c26
eSqadron <kubam0906@gmail.com>
2022-10-15 15:43:06 +0200

    three_legged_controller_added

--- ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
+++ ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
@@ -18,41 +18,42 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "walking_robot_interfaces/msg/step.hpp"
 
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
+class ThreeLegsController : public rclcpp::Node
 {
 public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+    ThreeLegsController()
+  : Node("three_legs_controller"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    publisher_ = this->create_publisher<walking_robot_interfaces::msg::Step>("step_1", 10);
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      5000ms, std::bind(&ThreeLegsController::timer_callback, this));
   }
 
 private:
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    auto message = walking_robot_interfaces::msg::Step();
+
+
     publisher_->publish(message);
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<walking_robot_interfaces::msg::Step>::SharedPtr publisher_;
   size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<ThreeLegsController>());
   rclcpp::shutdown();
   return 0;
 }
