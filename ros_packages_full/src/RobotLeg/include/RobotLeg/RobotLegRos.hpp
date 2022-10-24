ef6b87286b7d477f4e99287f0ae9576e7de6a6b6
eSqadron <kubam0906@gmail.com>
2022-10-24 18:46:27 +0200

    Update RobotLegRos.hpp

--- ros_packages_full/src/RobotLeg/include/RobotLeg/RobotLegRos.hpp
+++ ros_packages_full/src/RobotLeg/include/RobotLeg/RobotLegRos.hpp
@@ -48,6 +48,7 @@ public:
     void publish_servo_position(std::array<unsigned int, 3> new_servo_pos);
 
     std::array<int, 3> forward_kinematics(const std::array<int, 3> angles);
+    std::array<float, 3> inverse_kinematics(const std::array<float, 3> xyz_pos);
 
     //std::vector<std::array<unsigned int, 3>> interpolate_step(short unsigned int step_type, short int step_ang, int step_length, int step_height);
 
