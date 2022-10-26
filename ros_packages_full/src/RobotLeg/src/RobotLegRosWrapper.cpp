c11ec34978f55107613c56560583af1819108281
eSqadron <kubam0906@gmail.com>
2022-10-26 15:09:42 +0200

    Update RobotLegRosWrapper.cpp

--- ros_packages_full/src/RobotLeg/src/RobotLegRosWrapper.cpp
+++ ros_packages_full/src/RobotLeg/src/RobotLegRosWrapper.cpp
@@ -4,6 +4,8 @@
 
 #include "../include/RobotLeg/RobotLegRosWrapper.hpp"
 
+#include <string>
+
 void RobotLegRosWrapper::timer_callback(){
 //    try{
 //        if(robo_leg.is_step_being_performed()){
@@ -34,7 +36,7 @@ void RobotLegRosWrapper::step_callback(const geometry_msgs::msg::Point& msg){
 //    }
 
     robo_leg.move_leg_xyz(msg.x, msg.y, msg.z);
-    RCLCPP_INFO(this->get_logger(), robo_leg.temp_out_buffer_);
+    RCLCPP_INFO(this->get_logger(), std::to_string(robo_leg.temp_out_buffer_));
 
 }
 
