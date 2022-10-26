9da751946bd3b6e0a8008457f2b46349b2885b5c
eSqadron <kubam0906@gmail.com>
2022-10-26 15:52:35 +0200

    Update RobotLegRos.cpp

--- ros_packages_full/src/RobotLeg/src/RobotLegRos.cpp
+++ ros_packages_full/src/RobotLeg/src/RobotLegRos.cpp
@@ -132,7 +132,7 @@ std::array<float, 3> RobotLegRos::inverse_kinematics(const std::array<float, 3> 
     result_rad[0] = atan(y/x);
     result_rad[2] = PI - acos((pow(x-a_1_, 2) + pow(z - h_1_, 2) - pow(a_2_, 2) - pow(a_3_, 2))/(2*a_2_*a_3_));
     result_rad[1] = atan((z - h_1_)/(x - a_1_)) - asin(a_3_ * sin(result_rad[2])/(sqrt(pow(x - a_1_, 2) + pow(z - h_1_, 2))));
-    temp_out_buffer_ = result_rad[2];
+    temp_out_buffer_ = result_rad[0];
     return result_rad;
 }
 
