b66d75cbdb6d9c1073d6c4891d3a53b671064a07
eSqadron <kubam0906@gmail.com>
2022-10-28 23:13:27 +0200

    Update RobotLegRos.cpp

--- ros_packages_full/src/RobotLeg/src/RobotLegRos.cpp
+++ ros_packages_full/src/RobotLeg/src/RobotLegRos.cpp
@@ -139,6 +139,9 @@ void RobotLegRos::move_leg_xyz(float x, float y, float z){
     std::array<float, 3> inv_k = inverse_kinematics({x, y, z});
     temp_out_buffer_ = inv_k[2];
     temp_out_buffer2_ = rad2qns(temp_out_buffer_);
+
+    inv_k[1] = inv_k[1] + (35 * PI/180)
+
     publish_servo_position({rad2qns(inv_k[0]), rad2qns(-inv_k[1]), rad2qns(inv_k[2])});
 }
 
