9d685a91f5ee3ec251679aff3d836e076c30c89f
eSqadron <kubam0906@gmail.com>
2022-11-11 12:27:47 +0100

    Create tripod_launch.py

---
+++ ros_packages_full/launch/tripod_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='polulu_maestro',
+            executable='maestro_wrapper',
+            name='polulu_maestro'
+        ),
+        Node(
+            package='RobotLeg',
+            executable='robot_leg_wrapper',
+            name='RobotLeg'
+        ),
+        Node(
+            package='three_legs_controller',
+            executable='three_legs_controller',
+            name='three_legs_controller'
+        )
+
+    ])
