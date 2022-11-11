81e95d8f5afdc47336ed88ab72fac10c617c4b8a
eSqadron <kubam0906@gmail.com>
2022-11-11 16:22:06 +0100

    Update three_legs_controller.cpp

--- ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
+++ ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
@@ -41,7 +41,9 @@ public:
 
         timer_ = this->create_wall_timer(100ms, std::bind(&ThreeLegsController::timer_callback, this));
 
-        step_done_feedback_sub_ = this->create_subscription<std_msgs::msg::Bool>("step_done_1", 10, std::bind(&ThreeLegsController::step_done_callback, this, std::placeholders::_1));
+        step1_done_feedback_sub_ = this->create_subscription<std_msgs::msg::Bool>("step_done_1", 10, std::bind(&ThreeLegsController::step1_done_callback, this, std::placeholders::_1));
+        step2_done_feedback_sub_ = this->create_subscription<std_msgs::msg::Bool>("step_done_1", 10, std::bind(&ThreeLegsController::step2_done_callback, this, std::placeholders::_1));
+        step3_done_feedback_sub_ = this->create_subscription<std_msgs::msg::Bool>("step_done_1", 10, std::bind(&ThreeLegsController::step3_done_callback, this, std::placeholders::_1));
 
         current_step_stage_ = Idle;
 
@@ -51,7 +53,7 @@ public:
         message.z = -40;
         step_1_publisher_->publish(message);
         rclcpp::sleep_for(std::chrono::nanoseconds(6000000000));
-        is_step_stage_done_ = true;
+        is_step1_stage_done_ = true;
 
         step_2_publisher_->publish(message);
 
@@ -63,10 +65,10 @@ private:
         auto message = geometry_msgs::msg::Point();
         //robo_leg.set_physical_params(40, 55, 125, 180);
         
-        RCLCPP_INFO(this->get_logger(), (static_cast<std::string>("step_status: ") + std::to_string(is_step_stage_done_)).c_str());
+        RCLCPP_INFO(this->get_logger(), (static_cast<std::string>("step_status: ") + std::to_string(is_step1_stage_done_)).c_str());
 
         if(current_step_stage_ == Idle){
-            if(is_step_stage_done_){
+            if(is_step1_stage_done_ and is_step2_stage_done_){
                 message.x = 55 + 125 + 180;
                 message.y = 0;
                 message.z = -40;
@@ -77,7 +79,7 @@ private:
             }
         }
         else if(current_step_stage_ == Right_forward) {
-            if (is_step_stage_done_) {
+            if (is_step1_stage_done_ and is_step2_stage_done_){
                 message.x = 55 + 125;
                 message.y = 0;
                 message.z = -40;
@@ -91,11 +93,17 @@ private:
     }
 
 
-    void step_done_callback(const std_msgs::msg::Bool& msg){
-        is_step_stage_done_ = msg.data;
+    void step1_done_callback(const std_msgs::msg::Bool& msg){
+        is_step1_stage_done_ = msg.data;
+    }
+    void step2_done_callback(const std_msgs::msg::Bool& msg){
+        is_step2_stage_done_ = msg.data;
+    }
+    void step3_done_callback(const std_msgs::msg::Bool& msg){
+        is_step3_stage_done_ = msg.data;
     }
 
-    bool is_step_stage_done_;
+    bool is_step1_stage_done_;
     step_stage current_step_stage_;
 
     rclcpp::TimerBase::SharedPtr timer_;
@@ -103,7 +111,9 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr step_2_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr step_3_publisher_;
 
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr step_done_feedback_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr step1_done_feedback_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr step2_done_feedback_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr step3_done_feedback_sub_;
 };
 
 int main(int argc, char * argv[])
