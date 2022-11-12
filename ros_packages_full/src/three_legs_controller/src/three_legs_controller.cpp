7d96ba2a53a51a92b684e57ac999f057ac472cb4
eSqadron <kubam0906@gmail.com>
2022-11-12 11:42:36 +0100

    Update three_legs_controller.cpp

--- ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
+++ ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
@@ -52,6 +52,7 @@ public:
         message.y = 0;
         message.z = -40;
         step_1_publisher_->publish(message);
+        rclcpp::sleep_for(std::chrono::nanoseconds(4000000));
         step_2_publisher_->publish(message);
         rclcpp::sleep_for(std::chrono::nanoseconds(6000000000));
         is_step1_stage_done_ = true;
