220abe0b3bab77a5798b778036ac46f99e0b4533
eSqadron <kubam0906@gmail.com>
2022-11-30 19:36:04 +0100

    generator update

--- ros_packages_full/src/three_legs_controller/include/three_legs_controller/Generator3A.hpp
+++ ros_packages_full/src/three_legs_controller/include/three_legs_controller/Generator3A.hpp
@@ -23,9 +23,10 @@ class Generator3A {
 public:
     Generator3A(){
         restart_generator();
+        step_len_ = 10;
     }
 
-    void set_leg_default_positions(int pos1, int pos2, int pos3){
+    void set_leg_default_positions(float pos1, float pos2, float pos3){
         leg_pos_ = {pos1, pos2, pos3};
     }
 
@@ -34,28 +35,49 @@ public:
         current_right_leg_ = 0;
     }
 
-    void do_step(int ang){
-        if(current_step_stage_ == Idle){
+    std::tuple<std::array<float, 2>, int> do_step(int ang){
+        if(current_step_stage_ == R_for) {
+            current_step_stage_ = R_for;
+        }else if(current_step_stage_ == R_for){
             calculate_right_leg(ang);
-            TODO - znaleźć i przeliczyć ten kąt!!!
+            return std::make_tuple(calculate_endpoint_delta(ang, current_right_leg_), current_right_leg_);
+            current_step_stage_ = L_for;
         }
+        else if(current_step_stage_ == L_for){
+            calculate_right_leg(ang);
+            return std::make_tuple(calculate_endpoint_delta(ang, current_left_leg_), current_left_leg_);
+            current_step_stage_ = R_for;
+        }
+    }
 
+    std::array<float, 2> calculate_endpoint_delta(float ang, int leg_no){
+        float delta_x = l * sin(ang - leg_pos_[leg_no]);
+        float delta_y = l * cos(ang - leg_pos_[leg_no]);
+        return std::array<float, 2>{delta_x, delta_y};
     }
 
     void calculate_right_leg(int ang){
-        int leg_no = 1;
+        int leg_no = 0;
         for (int i = 1; i < 3; ++i) {
-            if(leg_pos_[i] - ang < leg_pos_[leg_no-1] - ang){
-                leg_no = i+1;
+            if(leg_pos_[i] - ang < leg_pos_[leg_no] - ang){
+                leg_no = i;
             }
         }
         current_right_leg_ = leg_no;
+        current_back_leg_ = (leg_no+1)%3;
+        current_left_leg_ = (leg_no+2)%3;
+
+
     }
 
 private:
     step_stage current_step_stage_;
     int current_right_leg_;
-    std::array<int, 3> leg_pos_;
+    int current_left_leg_;
+    int current_back_leg_;
+
+    std::array<float, 3> leg_pos_;
+    int step_len_;
 };
 
 
--- ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
+++ ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
@@ -22,6 +22,8 @@
 #include "std_msgs/msg/bool.hpp"
 #include "Generator3A.hpp"
 
+#define PI 3.14159265359
+
 using namespace std::chrono_literals;
 
 enum step_stage{
@@ -32,17 +34,7 @@ enum step_stage{
     Change_dir
 };
 
-int get_pos_from_leg(std::string leg_name){
-    auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(this, leg_name);
-    while (!parameters_client->wait_for_service(1s)) {
-        if (!rclcpp::ok()) {
-            RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
-            rclcpp::shutdown();
-        }
-        RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
-    }
-    return parameters_client->get_parameter("leg_pos");
-}
+
 
 class ThreeLegsController : public rclcpp::Node
 {
@@ -65,7 +57,7 @@ public:
         int leg_3_pos = get_pos_from_leg("leg_3");
 
 
-        gen_.set
+        gen_.set_leg_default_positions(PI * 60/180, PI, PI * 300/180);
 
         auto message = geometry_msgs::msg::Point();
         message.x = 55 + 125 + 180;
@@ -78,6 +70,7 @@ public:
         is_step1_stage_done_ = true;
         is_step2_stage_done_ = true;
         is_step3_stage_done_ = true;
+        leg_no_step_done_ = {true, true, true};
     }
 
 private:
@@ -85,6 +78,10 @@ private:
     {
         auto message = geometry_msgs::msg::Point();
         //robo_leg.set_physical_params(40, 55, 125, 180);
+
+        std::tuple<std::array<float, 2>, int> do_step_result;
+        std::array<float, 2> endpoint_shift;
+        int moving_leg;
         
         
         RCLCPP_INFO(this->get_logger(), std::to_string(is_step1_stage_done_ && is_step2_stage_done_).c_str());
@@ -110,22 +107,45 @@ private:
             }
         }
 
+        do_step_result = gen_.do_step(0);
+        endpoint_shift = std::get<0>(do_step_result);
+        moving_leg = std::get<1>(do_step_result);
+
+
+
+    }
+
+    int get_pos_from_leg(std::string leg_name){
+        auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(this, leg_name);
+        while (!parameters_client->wait_for_service(1s)) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+                rclcpp::shutdown();
+            }
+            RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
+        }
+        return parameters_client->get_parameter("leg_pos");
     }
 
 
     void step1_done_callback(const std_msgs::msg::Bool& msg){
         is_step1_stage_done_ = msg.data;
+        leg_no_step_done_[0] = msg.data;
     }
     void step2_done_callback(const std_msgs::msg::Bool& msg){
         is_step2_stage_done_ = msg.data;
+        leg_no_step_done_[1] = msg.data;
     }
     void step3_done_callback(const std_msgs::msg::Bool& msg){
         is_step3_stage_done_ = msg.data;
+        leg_no_step_done_[2] = msg.data;
     }
 
     bool is_step1_stage_done_;
     bool is_step2_stage_done_;
     bool is_step3_stage_done_;
+    std::array<bool, 3> leg_no_step_done_;
+
     step_stage current_step_stage_;
 
     Generator3A gen_;
@@ -134,6 +154,8 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr step_1_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr step_2_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr step_3_publisher_;
+
+    //std::array<rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr, 3>
 
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr step1_done_feedback_sub_;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr step2_done_feedback_sub_;
