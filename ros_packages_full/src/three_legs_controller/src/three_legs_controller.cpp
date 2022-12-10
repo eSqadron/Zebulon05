a1aadcf9d75fa75fe05676c32ced5f0696eda82c
eSqadron <kubam0906@gmail.com>
2022-12-10 16:10:39 +0100

    Update three_legs_controller.cpp

--- ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
+++ ros_packages_full/src/three_legs_controller/src/three_legs_controller.cpp
@@ -107,7 +107,7 @@ private:
 
         if(current_step_stage_ == Idle_){
             if(is_step3_stage_done_){
-                message.x = 150;
+                message.x = 140;
                 message.y = 0;
                 message.z = -100;
                 step_3_publisher_->publish(message);
@@ -117,7 +117,7 @@ private:
         }
         else if(current_step_stage_ == Right_forward) {
             if (is_step1_stage_done_){
-                message.x = 150;
+                message.x = 140;
                 message.y = 0;
                 message.z = -100;
                 step_1_publisher_->publish(message);
@@ -127,7 +127,7 @@ private:
         }
         else if(current_step_stage_ == Middle_back) {
             if (is_step2_stage_done_){
-                message.x = 150;
+                message.x = 140;
                 message.y = 0;
                 message.z = -100;
                 step_2_publisher_->publish(message);
